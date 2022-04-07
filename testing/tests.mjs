2e825bc8a9f26f92159658d61ecd72d235b9c6cc
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-07 14:31:16 +0200

    thingysync push

---
+++ testing/tests.mjs
@@ -0,0 +1,82 @@
+// global.window = {}
+
+import * as but from "../output/index.js"
+import {run, mark} from "micro-bmark"
+
+const constArray =  new Uint8Array(10)
+constArray.fill(32)
+constArray[9] = 42
+
+const constArrayBuffer = constArray.buffer
+const constBuffer = Buffer.from(constArrayBuffer)
+const constHex = but.bytesToHex(constArray)
+const constUtf8 = "HelloWörld!"
+
+const samples = 1000
+let result
+
+const buffer = but.utf8ToBytes(constUtf8)
+const newstring = but.bytesToUtf8(buffer)
+console.log(newstring) 
+
+run(async () => {
+    
+    // bytesToBigInt
+    await mark('bytesToBigInt Uint8Array', samples, () => {
+        but.bytesToBigInt(constArray)
+    });
+
+    await mark('bytesToBigInt ArrayBuffer', samples, () => {
+        but.bytesToBigInt(constArrayBuffer)
+    });
+
+    await mark('bytesToBigInt Buffer', samples, () => {
+        but.bytesToBigInt(constBuffer)
+    });
+
+
+    // bytesToHex
+    await mark('bytesToHex Uint8Array', samples, () => {
+        but.bytesToHex(constArray)
+    });
+
+    await mark('bytesToHex ArrayBuffer', samples, () => {
+        but.bytesToHex(constArrayBuffer)
+    });
+
+    await mark('bytesToHex Buffer', samples, () => {
+        but.bytesToHex(constBuffer)
+    });
+
+
+    // HexToBytes
+    await mark('hexToBytes', samples, () => {
+        but.hexToBytes(constHex)
+    });
+
+    // Utf8ToBytes
+    await mark('utf8ToBytes', samples, () => {
+        but.utf8ToBytes(constUtf8)
+    });
+
+
+    // bytesToUtf8
+    await mark('bytesToUtf8 Uint8Array', samples, () => {
+        but.bytesToUtf8(constArray)
+    });
+
+    await mark('bytesToUtf8 ArrayBuffer', samples, () => {
+        but.bytesToUtf8(constArrayBuffer)
+    });
+
+    await mark('bytesToUtf8 Buffer', samples, () => {
+        but.bytesToUtf8(constBuffer)
+    });
+
+
+    // // Log current RAM
+    // bench.logMem();
+   
+    // // Get current time in nanoseconds
+    // bench.getTime();
+  });
